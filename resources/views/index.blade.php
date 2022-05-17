09168d989d4f24cf5adb9a77f2474e7b4d7cd2fd
yamunna21 <65716763+yamunna21@users.noreply.github.com>
2022-05-17 16:19:40 +0800

    Update index.blade.php
    Update Raya Celebration Banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,8 +17,8 @@ TiNKA - Education. Everywhere.
             
             <div data-aos="fade-up" data-aos-delay="600">
               <div class="text-center text-lg-start">
- <!--                <a href="https://forms.gle/3NH2XQAhEU4mSoeb7" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center"> -->
-<!--                   <span>{{ __("Register Now") }}</span>
+<!--                 <a href="https://forms.gle/3NH2XQAhEU4mSoeb7" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+ -->                  <span>{{ __("Register Now") }}</span>
                   <i class="bi bi-arrow-right"></i> -->
                 </a>
               </div>
